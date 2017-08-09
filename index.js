f5ce2f9c7f4a192ad125576f57bc91e2dc0b5575
Nah.Myself( ) <larrypaulingjr@gmail.com>
2017-08-09 17:59:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -55,3 +55,61 @@ function redraw() {
   context.drawImage(images["AstroboyHead"], x - 10, y - 125);
   context.drawImage(images["AstroboyHair"], x - 37, y - 138);
 }
+
+/* Draw Eyes */
+function redraw() {
+  ...
+  drawEllipse(x + 47, y - 68, 8, 14); // Left Eye
+  drawEllipse(x + 58, y - 68, 8, 14); // Right Eye
+}
+
+function drawEllipse(centerX, centerY, width, height) {
+	
+  context.beginPath();
+  
+  context.moveTo(centerX, centerY - height/2);
+  
+  context.bezierCurveTo(
+    centerX + width/2, centerY - height/2,
+    centerX + width/2, centerY + height/2,
+    centerX, centerY + height/2);
+
+  context.bezierCurveTo(
+    centerX - width/2, centerY + height/2,
+    centerX - width/2, centerY - height/2,
+    centerX, centerY - height/2);
+ 
+  context.fillStyle = "black";
+  context.fill();
+  context.closePath();	
+}
+
+function redraw() {
+  ...
+  canvas.width = canvas.width; // clears the canvas 
+  
+  drawEllipse(x + 40, y + 29, 160, 6);
+  ...
+}
+  
+/* Breathe Dynamic */
+  var breathInc = 0.1;
+var breathDir = 1;
+var breathAmt = 0;
+var breathMax = 2;
+var breathInterval = setInterval(updateBreath, 1000 / fps);
+  
+  function updateBreath() { 
+                        
+  if (breathDir === 1) {  // breath in
+    breathAmt -= breathInc;
+    if (breathAmt < -breathMax) {
+      breathDir = -1;
+    }
+  } else {  // breath out
+    breathAmt += breathInc;
+    if(breathAmt > breathMax) {
+      breathDir = 1;
+    }
+  }
+}
